3aa5417eb9d130ee5bf91300f23ed10242f52696
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-02-26 23:16:57 +0200

    Update Params.js

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = 'f49f8557-a411-439f-9d00-30d63a7e1c03'; // ключ OneSignal
   static appID = '6742375380'; // ID додатку в сторі
   static keyApps = 'T9h7tLijQjDdZXpvgxh44L'; // ключ Appsflyer
-  static targetDate = '2025-02-27'; // дата закінчення відльожки
+  static targetDate = '2025-02-28'; // дата закінчення відльожки
   static bodyLin = 'https://miraculous-exalted-happiness.space/YfyJw24B'; // тіло лінки з якою будемо працювати
 }
